d0c9703a5bc018eb50e32409ac93a84aca7fc169
ArnaudDoubremelle <a-r-n-a-u-d-@hotmail.fr>
2015-12-19 19:58:33 +0100

    contact

--- src/front-page.php
+++ src/front-page.php
@@ -5,4 +5,5 @@ get_template_part( 'sections/about_us' );
 get_template_part('sections/team');
 get_template_part('sections/data_management');
 get_template_part('sections/products');
+get_template_part('sections/contact');
 get_footer();
---
+++ src/sections/contact.php
@@ -0,0 +1,30 @@
+<section id='contact' class="egiis-section">
+    <h2 id='contact-title'>contact</h2>
+    <p id="contact-desc">
+        Un produit que nous proposons vous intéresse ?<br>
+        Vous souhaitez nous proposer une mission de conseil ou de développement ?<br>
+        Vous souhaitez utiliser le moteur d'extraction de connaissances K.O.A.I ?
+    </p>
+
+    <form>
+        <div id="contact-glob">
+            <div id="contact-left">
+                <div class="champ champ-bot">
+                    <label for="name">Nom</label>
+                    <input required="required" type="text" id="name" name="name">
+                </div>
+                <div class="champ">
+                    <label for="email">Mail</label>
+                    <input required="required" type="email" id="email" name="email">
+                </div>
+            </div>
+            <div id="contact-right">
+                <div class="champ">
+                    <label for="message">Message</label>
+                    <textarea required="required" type="text" id="message" name="message"></textarea>
+                </div>
+            </div>
+            <button class="contact-btn" type="submit">Envoyer</button>
+        </div>
+    </form>
+</section>
