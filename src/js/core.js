42c27322b49a70d5c2e7fffac8b6fbc858299b6d
Romain Bellande <romain.bellande@orange.fr>
2015-12-20 16:46:47 +0100

    update scrollomatic

--- src/js/core.js
+++ src/js/core.js
@@ -77,5 +77,56 @@ enquire.register("screen and (max-width:768px)", {
 
 });
 
+enquire.register("screen and (min-width:768px)", {
+
+    // OPTIONAL
+    // If supplied, triggered when a media query matches.
+    match : function() {
+      $('#menu-header li a').scrollomatic({
+        offset: 130
+      });
+      var menu = document.getElementById('responsive-menu'),
+          header = document.getElementById('masthead');
+      if (menu.className.indexOf( 'toggled' ) !== -1) {
+        menu.className = menu.className.replace(' toggled', '');
+        header.className = header.className.replace(' mobile-activate-menu', '');
+        header.className += ' mobile-disactivate-menu';
+      }
+    },
+
+    // OPTIONAL
+    // If supplied, triggered when the media query transitions
+    // *from a matched state to an unmatched state*.
+    unmatch : function() {
+      $('#menu-header li a').scrollomatic();
+      $('#menu-header li a').on('click',function(){
+        var menu = document.getElementById('responsive-menu'),
+            header = document.getElementById('masthead');
+        if (menu.className.indexOf( 'toggled' ) !== -1) {
+          menu.className = menu.className.replace(' toggled', '');
+          header.className = header.className.replace(' mobile-activate-menu', '');
+          header.className += ' mobile-disactivate-menu';
+        }
+      });
+      $('#header-shortcut').scrollomatic();
+
+    },
+
+    // OPTIONAL
+    // If supplied, triggered once, when the handler is registered.
+    setup : function() {},
+
+    // OPTIONAL, defaults to false
+    // If set to true, defers execution of the setup function
+    // until the first time the media query is matched
+    deferSetup : true,
+
+    // OPTIONAL
+    // If supplied, triggered when handler is unregistered.
+    // Place cleanup code here
+    destroy : function() {}
+
+});
+
   });
 }(jQuery));
